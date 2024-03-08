82e09f09edfa5310d5f307dab68fa35323620394
fauzan <fauzanalghifari963@gmail.com>
2024-03-09 00:28:37 +0700

    initial commit

---
+++ Dashboard/main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# getting the data
+data = pd.read_csv('data.csv')
+
+"""
+    Function definitions
+"""
+
+def plot_compare_on_column(df, column):
+    # function definition here
+    pass
+
+def plot_weather_on_column(df, column, weather):
+    # function definition here
+    pass
+
+def plot_traffic_active_weather(df, weather):
+    # function definition here
+    pass
+
+""""
+"""
